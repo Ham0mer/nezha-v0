3a7b663b3bfc3e553d40cbfaf82d3047f3875eb7
rong6 <109727098+rong6@users.noreply.github.com>
2025-02-12 20:06:47 +0800

    fix: Temporary fix for Chinese mainland unable to download agent problem

--- install.sh
+++ install.sh
@@ -375,7 +375,7 @@ install_agent() {
     if [ -z "$CN" ]; then
         NZ_AGENT_URL="https://${GITHUB_URL}/nezhahq/agent/releases/download/${_version}/nezha-agent_linux_${os_arch}.zip"
     else
-        NZ_AGENT_URL="https://${GITHUB_URL}/naibahq/agent/releases/download/${_version}/nezha-agent_linux_${os_arch}.zip"
+        NZ_AGENT_URL="https://go.xiaobai.mom/https://github.com/nezhahq/agent/releases/download/${_version}/nezha-agent_linux_${os_arch}.zip"
     fi
 
     _cmd="wget -t 2 -T 60 -O nezha-agent_linux_${os_arch}.zip $NZ_AGENT_URL >/dev/null 2>&1"
